e46bfdd377ff9f8f4edc9897756a450815863f27
S0nFra <80450457+S0nFra@users.noreply.github.com>
2021-11-09 17:28:35 +0100

    Hello Fisciano

--- Prova/src/main/java/it/unisa/prova/Main.java
+++ Prova/src/main/java/it/unisa/prova/Main.java
@@ -10,8 +10,9 @@ package it.unisa.prova;
  */
 public class Main {
     public static void main(String args[]) {
-        System.out.println("Hello, world");
         System.out.println("Hello Fisciano");
+        System.out.println("Hello, world");
+        
     }
     
 }
